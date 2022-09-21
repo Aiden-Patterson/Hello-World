39bdc1f5b44ba8beabb0dc993a8aefdba77f5c89
aiden-patterson <pat18001@byui.edu>
2022-09-21 14:01:20 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.Write("This is a test to see");
+        Console.WriteLine(" how C# works"); 
+        Console.Write("What is the first value: ");
+        string x = Console.ReadLine();
+        Console.Write("What is the second value: ");
+        string y = Console.ReadLine();
+        Console.WriteLine("The solution to the problem " + x + " + " + y + " is " + (x + y));
+        int xVal = Convert.ToInt32(x);
+        int yVal = Convert.ToInt32(y);
+        Console.WriteLine("Wait that's not right, the solution is " + (xVal + yVal));
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Overview
+
+This is a classic Hello world. I am using it do test how git and github work so that I know what I am doing
+
+{Provide a link to your YouTube demonstration.  It should be a one minute demo of the software running and a walkthrough of the code.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+* C#
+* Visual Studio Code
+* Git / GitHub
+
+{Describe the programming language that you used}
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [Git](https://git-scm.com/)
+* [Web Site Name](http://url.link.goes.here)
